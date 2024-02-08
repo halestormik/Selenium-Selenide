3ffdb18be1ccf93e9fe72d1c652cb199c90eb630
NetoIQA75 <all-slavin@yandex.ru>
2024-02-08 13:21:39 +0300

    Make corrections

--- src/test/java/CardOrderTest.java
+++ src/test/java/CardOrderTest.java
@@ -7,11 +7,15 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class CardOrderTest {
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999/");
+    }
 
     @Test
-    public void ShouldTestFormWithCorrectData() { // проверка корректной работы формы при верных данных
-        open("http://localhost:9999/");
-        SelenideElement form = $("#root");
+    public void shouldTestFormWithCorrectData() { // проверка корректной работы формы при верных данных
+
+        SelenideElement form = $("form");
         form.$("[data-test-id=name] input").setValue("Василий");
         form.$("[data-test-id=phone] input").setValue("+79185463782");
         form.$("[data-test-id=agreement]").click();
@@ -20,75 +24,70 @@ public class CardOrderTest {
     }
 
     @Test
-    public void ShouldTestFormWithEmptyNameField() { // проверка работы формы при пустом поле ввода имени
+    public void shouldTestFormWithEmptyNameField() { // проверка работы формы при пустом поле ввода имени
 
-        open("http://localhost:9999/");
-        SelenideElement form = $("#root");
+        SelenideElement form = $("form");
         form.$("[data-test-id=name] input").setValue("");
         form.$("[data-test-id=phone] input").setValue("+79185463782");
         form.$("[data-test-id=agreement]").click();
         form.$("button").click();
 
         $("[data-test-id=name] .input__sub").shouldHave(exactText("Поле обязательно для заполнения")); // проверка текста ошибки
-        String fieldColor = $("[data-test-id=name] .input__sub").getCssValue("color"); // получение цвета текста ошибки
-        Assertions.assertEquals("rgba(255, 92, 92, 1)", fieldColor.trim()); // сравнение цвета текста ошибки с красным
+        boolean b = $("[data-test-id=name].input_invalid").isDisplayed(); // проверка наличия класса input_invalid
+        Assertions.assertTrue(b);
     }
 
     @Test
-    public void ShouldTestFormWithIncorrectNameField() { // проверка работы формы при некорректных значениях поля ввода имени
+    public void shouldTestFormWithIncorrectNameField() { // проверка работы формы при некорректных значениях поля ввода имени
 
-        open("http://localhost:9999/");
-        SelenideElement form = $("#root");
+        SelenideElement form = $("form");
         form.$("[data-test-id=name] input").setValue("2212");
         form.$("[data-test-id=phone] input").setValue("+79185463782");
         form.$("[data-test-id=agreement]").click();
         form.$("button").click();
 
         $("[data-test-id=name] .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.")); // проверка текста ошибки
-        String fieldColor = $("[data-test-id=name] .input__sub").getCssValue("color"); // получение цвета текста ошибки
-        Assertions.assertEquals("rgba(255, 92, 92, 1)", fieldColor.trim()); // сравнение цвета текста ошибки с красным
+        boolean b = $("[data-test-id=name].input_invalid").isDisplayed(); // проверка наличия класса input_invalid
+        Assertions.assertTrue(b);
     }
 
     @Test
-    public void ShouldTestFormWithEmptyPhoneField() { // проверка работы формы при пустом поле ввода телефона
+    public void shouldTestFormWithEmptyPhoneField() { // проверка работы формы при пустом поле ввода телефона
 
-        open("http://localhost:9999/");
-        SelenideElement form = $("#root");
+        SelenideElement form = $("form");
         form.$("[data-test-id=name] input").setValue("Василий");
         form.$("[data-test-id=phone] input").setValue("");
         form.$("[data-test-id=agreement]").click();
         form.$("button").click();
 
         $("[data-test-id=phone] .input__sub").shouldHave(exactText("Поле обязательно для заполнения")); // проверка текста ошибки
-        String fieldColor = $("[data-test-id=phone] .input__sub").getCssValue("color"); // получение цвета текста ошибки
-        Assertions.assertEquals("rgba(255, 92, 92, 1)", fieldColor.trim()); // сравнение цвета текста ошибки с красным
+        boolean b = $("[data-test-id=phone].input_invalid").isDisplayed(); // проверка наличия класса input_invalid
+        Assertions.assertTrue(b);
     }
 
     @Test
-    public void ShouldTestFormWithIncorrectPhoneField() { // проверка работы формы при некорректных значениях поля ввода телефона
+    public void shouldTestFormWithIncorrectPhoneField() { // проверка работы формы при некорректных значениях поля ввода телефона
 
-        open("http://localhost:9999/");
-        SelenideElement form = $("#root");
+        SelenideElement form = $("form");
         form.$("[data-test-id=name] input").setValue("Василий");
         form.$("[data-test-id=phone] input").setValue("Петрович");
         form.$("[data-test-id=agreement]").click();
         form.$("button").click();
 
         $("[data-test-id=phone] .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.")); // проверка текста ошибки
-        String fieldColor = $("[data-test-id=phone] .input__sub").getCssValue("color"); // получение цвета текста ошибки
-        Assertions.assertEquals("rgba(255, 92, 92, 1)", fieldColor.trim()); // сравнение цвета текста ошибки с красным
+        boolean b = $("[data-test-id=phone].input_invalid").isDisplayed(); // проверка наличия класса input_invalid
+        Assertions.assertTrue(b);
     }
 
     @Test
-    public void ShouldTestFormWithoutClickingOnCheckbox() { // проверка работы формы, если не нажат чекбокс
+    public void shouldTestFormWithoutClickingOnCheckbox() { // проверка работы формы, если не нажат чекбокс
 
-        open("http://localhost:9999/");
-        SelenideElement form = $("#root");
+        SelenideElement form = $("form");
         form.$("[data-test-id=name] input").setValue("Василий");
         form.$("[data-test-id=phone] input").setValue("+79185463782");
         form.$("button").click();
 
-        String fieldColor = $("[data-test-id=agreement] .checkbox__text").getCssValue("color"); // получение цвета текста ошибки
-        Assertions.assertEquals("rgba(255, 92, 92, 1)", fieldColor.trim()); // сравнение цвета текста ошибки с красным
+        boolean b = $("[data-test-id=agreement].input_invalid").isDisplayed(); // проверка наличия класса input_invalid
+        Assertions.assertTrue(b);
     }
 }
